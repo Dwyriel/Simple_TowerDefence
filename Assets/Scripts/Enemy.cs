3bc640104d2f7c9374173504efd76cb882468273
Dwyriel <Dwyriel333@hotmail.com>
2020-06-10 09:18:26 -0300

    Enemy collider & health

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int hitsToDie = 5;
+    bool isDestroyed = false;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        hitsToDie--;
+        if (!isDestroyed && hitsToDie <= 0)
+        {
+            TriggerDeath();
+        }
+    }
+
+    private void TriggerDeath()
+    {
+        isDestroyed = true;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan, targetEnemy;
+
+    void Update()
+    {
+        lootAtEnemy();
+    }
+
+    void lootAtEnemy()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+
+}
